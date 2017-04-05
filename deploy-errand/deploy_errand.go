9c8e3009955e51cbce52cd44281cfd47e7ec444f
Glyn Normington <gnormington@pivotal.io>
2017-04-05 11:37:53 +0100

    Unmarshall JSON in errand

--- deploy-errand/deploy_errand.go
+++ deploy-errand/deploy_errand.go
@@ -6,11 +6,40 @@ import (
 
 	"code.cloudfoundry.org/commandrunner/linux_command_runner"
 	"github.com/glyn/ergo/cf"
+	"io/ioutil"
+	"encoding/json"
 )
+
+type DeploymentProperties struct {
+	SpringCloudBroker SpringCloudBrokerProperties `json:"spring_cloud_broker"`
+}
+
+type SpringCloudBrokerProperties struct {
+	CF                    CFProperties
+}
+
+type CFProperties struct {
+	Sample string
+}
 
 func main() {
 	fmt.Println("* Starting deploy errand")
-	fmt.Printf("$SAMPLE=%s\n", os.Getenv("SAMPLE"))
+
+	jsonPath := os.Args[1]
+	j, err := ioutil.ReadFile(jsonPath)
+	if err != nil {
+		fmt.Printf("Failed to read JSON properties file: %s\n", err)
+		os.Exit(1)
+	}
+
+	var props DeploymentProperties
+	err = json.Unmarshal(j, &props)
+	if err != nil {
+		fmt.Printf("Failed to unmarshall JSON properties: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("spring_cloud_broker.cf.sample=%s\n", props.SpringCloudBroker.CF.Sample)
 
 	cf := cf.New(linux_command_runner.New())
 	v, err := cf.DisplayCfVersion()
